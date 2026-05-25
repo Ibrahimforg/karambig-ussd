94727865dd4e537e87f4a7479d2e382004bba02f
ibrahimforg <ibrahimforgo59@gmail.com>
2026-05-25 23:28:17 +0000

    Add detailed SMS content logging

--- services/smsService.js
+++ services/smsService.js
@@ -4,15 +4,16 @@ const logger                 = require('../utils/logger');
 
 async function envoyerSMS(telephone, message) {
   const texte = tronquer(nettoyer(message), 800);
+  logger.info(`SMS preparation - Telephone: ${telephone}, Contenu: "${texte}"`);
   try {
     const result = await sms.send({
       to: [telephone], message: texte,
       from: process.env.AT_SENDER_ID || 'KarambigRoogo',
     });
-    logger.info(`SMS envoye a ${telephone}`);
+    logger.info(`SMS envoye a ${telephone} - Contenu: "${texte}"`);
     return result;
   } catch (err) {
-    logger.error(`Erreur SMS a ${telephone}: ${err.message}`);
+    logger.error(`Erreur SMS a ${telephone}: ${err.message} - Contenu: "${texte}"`);
     throw err;
   }
 }
